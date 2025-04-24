527068eec4abc4dcfa43d905ad82715fe272f5a4
benedictwallace <benedictwallace01@gmail.com>
2025-04-24 16:48:30 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # amls2_coursework
+main.py may not run, run the seperate pre_train and main_vgg files for better results.
+
+RUN main.py with PRETRAIN = True to pretrain the model, otherwise only the GAN phase will run
+change the track with TRACK = 1 or 2.
+
+pre_train.py is just the pretraining for track 1.
+main_vgg.py is just the GAN phase for track 1.
+
+pre_train_B.py is just the pretraining for track 2.
+main_vgg_B.py is just the GAN phase for track 2.
+
+display_model.py contains the code to stitch patches together and run them through the models, be careful with the scales when changing tracks.
+
+The datasets need to be imported to the /datasets folder from https://data.vision.ee.ethz.ch/cvl/DIV2K/
--- pre_train.py
+++ pre_train.py
@@ -6,119 +6,57 @@ from tqdm import tqdm
 import os
 import matplotlib.pyplot as plt
 from keras import backend as K
+import random
+from model import SRGANTrainer, build_discriminator, build_generator, build_vgg_for_content_loss
+from fetch_and_crop import crop_patch_pairs, create_batches
 
-def upscale(initial):
-    up_model = layers.Conv2D(256, (3,3), padding="same")(initial)
-    up_model = layers.UpSampling2D(size=2)(up_model)
-    up_model = layers.PReLU(shared_axes=[1,2])(up_model)
+# -----------------------------
+# Create random crops for training data.
+n_random_crops_train = 15  # number of crops per training image pair
 
-    return up_model
+n_random_crops_val = 1  # number of crops per validation image pair
 
-def residual_block(initial):
-    res = layers.Conv2D(64, (3,3), padding="same")(initial)
-    res = layers.BatchNormalization(momentum=0.5)(res)
-    res = layers.PReLU(shared_axes = [1,2])(res)
-    res = layers.Conv2D(64, (3,3), padding="same")(res)
-    res = layers.BatchNormalization(momentum=0.5)(res)
+train_lr_cropped, train_hr_cropped, val_lr_cropped, val_hr_cropped = crop_patch_pairs(n_random_crops_train, n_random_crops_val)
 
-    return layers.add([initial, res])
+# -----------------------------
+# Create batches from the cropped patches.
+batch_size = 32
 
-def build_generator(input_shape, res_blocks):
-    
-    gen_in = layers.Input(shape=input_shape)
+#train_lr_batches, train_hr_batches = create_batches(train_lr_cropped, train_hr_cropped, batch_size)
+val_lr_batches, val_hr_batches = create_batches(val_lr_cropped, val_hr_cropped, batch_size)
 
-    gen = layers.Conv2D(64, (9,9), padding="same")(gen_in)
-    gen = layers.PReLU(shared_axes=[1,2])(gen)
+lr_shape = (32, 32, 3)
 
-    temp = gen
+hr_shape = (128, 256, 3)
 
-    for i in range(0, res_blocks):
-        gen = residual_block(gen)
-
-    gen = layers.Conv2D(64, (3,3), padding="same")(gen)
-    gen = layers.BatchNormalization(momentum=0.5)(gen)
-    gen = layers.add([gen, temp])
-
-    gen = upscale(gen)
-    gen = upscale(gen)
-    gen = upscale(gen)
-
-    op = layers.Conv2D(3, (9,9), padding="same", activation="sigmoid")(gen)
-
-    model = Model(inputs=gen_in, outputs=op)
-
-    return model
-
-
-generator = build_generator(res_blocks=8, input_shape=(32, 32, 3))
+generator = build_generator(res_blocks=16, input_shape=lr_shape, final_activation="sigmoid")
 
 generator.compile(
-    loss=tf.keras.losses.MeanSquaredError(), 
+    loss=tf.keras.losses.MeanSquaredError(),
     optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4, beta_1=0.5)
 )
 
+epochs = 200
 
-# Pull cropped images and batch-------------------------------------
-
-
-# Paths to your cropped directories
-lr_cropped_dir = "amls2_coursework/datasets/DIV2K_train_LR_cropped"
-hr_cropped_dir = "amls2_coursework/datasets/DIV2K_train_HR_cropped"
-
-# Get list of file names
-lr_cropped_files = sorted(os.listdir(lr_cropped_dir))
-hr_cropped_files = sorted(os.listdir(hr_cropped_dir))
-
-# Lists to store the loaded images
-lr_cropped_images = []
-hr_cropped_images = []
-
-# Read each PNG, decode, and convert to float32 in [0,1]
-for lr_file, hr_file in zip(lr_cropped_files, hr_cropped_files):
-    lr_path = os.path.join(lr_cropped_dir, lr_file)
-    hr_path = os.path.join(hr_cropped_dir, hr_file)
-
-    # Read LR image
-    lr_img = tf.io.read_file(lr_path)
-    lr_img = tf.image.decode_png(lr_img, channels=3)
-    lr_img = tf.image.convert_image_dtype(lr_img, tf.float32)  # now in [0,1]
-
-    # Read HR image
-    hr_img = tf.io.read_file(hr_path)
-    hr_img = tf.image.decode_png(hr_img, channels=3)
-    hr_img = tf.image.convert_image_dtype(hr_img, tf.float32)  # now in [0,1]
-
-    lr_cropped_images.append(lr_img)
-    hr_cropped_images.append(hr_img)
-
-
-# Now create batches
-batch_size = 8
-
-train_lr_batches = []
-train_hr_batches = []
-
-for i in range(0, len(lr_cropped_images), batch_size):
-    # Slice out a batch of LR / HR patches
-    lr_batch = lr_cropped_images[i : i + batch_size]
-    hr_batch = hr_cropped_images[i : i + batch_size]
-
-    # Stack them into (batch_size, H, W, 3)
-    lr_batch = np.stack(lr_batch, axis=0)
-    hr_batch = np.stack(hr_batch, axis=0)
-    
-    train_lr_batches.append(lr_batch)
-    train_hr_batches.append(hr_batch)
-
-#--------------------------------------------------------------
-
-
-
-epochs = 30
+train_loss_history = []
+val_loss_history = []
 
 for epoch in range(epochs):
     epoch_losses = []
-    
+    val_losses = []
+    # Shuffle
+    combined = list(zip(train_lr_cropped, train_hr_cropped))
+    random.shuffle(combined)
+    # Unzip the list back into LR / HR arrays
+    train_lr_cropped_shuffled, train_hr_cropped_shuffled = zip(*combined)
+
+    # Rebatch
+    train_lr_batches, train_hr_batches = create_batches(
+        train_lr_cropped_shuffled, 
+        train_hr_cropped_shuffled, 
+        batch_size
+    )
+
     for b in tqdm(range(len(train_lr_batches))):
         lr_imgs = train_lr_batches[b]  # shape [batch_size, 32, 32, 3]
         hr_imgs = train_hr_batches[b]  # shape [batch_size, 256, 256, 3]
@@ -126,22 +64,33 @@ for epoch in range(epochs):
         # Train generator on this batch
         loss = generator.train_on_batch(lr_imgs, hr_imgs)
         epoch_losses.append(loss)
+
+        
+    for b in range(len(val_hr_batches)):
+        lr_imgs_val = val_lr_batches[b]
+        hr_imgs_val = val_hr_batches[b]
+        
+        loss = generator.test_on_batch(lr_imgs, hr_imgs)
+        val_losses.append(loss)
     
-    print(f"Pretrain Epoch {epoch+1}/{epochs}, G_MSE_Loss: {np.mean(epoch_losses):.4f}")
+    train_loss_epoch = np.mean(epoch_losses)
+    val_loss_epoch   = np.mean(val_losses)
 
-generator.save_weights("generator_pretrained.keras")
+    train_loss_history.append(train_loss_epoch)
+    val_loss_history.append(val_loss_epoch)
 
-sample_lr_img = lr_cropped_images[0] 
-sample_hr_img = hr_cropped_images[0]
+    print(f"Pretrain Epoch {epoch+1}/{epochs}, G_MSE_Loss: {train_loss_epoch:.4f}, val_loss: {val_loss_epoch:.4f}")
 
-input_lr = np.expand_dims(sample_lr_img, axis=0)
-upscaled_img = generator.predict(input_lr)
-upscaled_img = np.squeeze(upscaled_img, axis=0)
+generator.save_weights("generator_pretrained.weights.h5")
 
-resized_lr = tf.image.resize(sample_lr_img, (256, 256)).numpy()
-combined_img = np.concatenate([resized_lr, upscaled_img, sample_hr_img], axis=1)
 
-plt.figure()
-plt.imshow(combined_img)
-plt.title("Left: LR resized | Middle: SRGAN Output | Right: Original HR")
-plt.show()
+plt.figure(figsize=(8, 5))
+plt.plot(range(1, epochs+1), train_loss_history, label='Train Loss')
+plt.plot(range(1, epochs+1), val_loss_history,   label='Validation Loss')
+plt.title('Pre-training Losses')
+plt.xlabel('Epoch')
+plt.ylabel('Loss (MSE)')
+plt.legend()
+plt.tight_layout()
+plt.savefig('pre_training_losses.png')
+plt.close()
